2da9d04444f9af59ea4a419879bba9fc21c58236
AnsariAhmedFrontEnd <117654588+AnsariAhmedFrontEnd@users.noreply.github.com>
2024-10-14 19:29:32 +0530

    Update keys.js

--- backend/keys.js
+++ backend/keys.js
@@ -1,7 +1,3 @@
 
-
-module.exports = {
-  mongoUrl :
-  "mongodb+srv://am27043:am27043@cluster0.vgg8pdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  jwt_secret : "adfjkjadfhkjas"
-}
+  mongoUrl=mongodb+srv://am27043:am27043@cluster0.vgg8pdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+  jwt_secret=adfjkjadfhkjas
